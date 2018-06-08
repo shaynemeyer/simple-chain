0a6aad5e9fb373cc3b16fd4aea8b34a1d371ecb7
Shayne Meyer <shayne.meyer@gmail.com>
2018-06-07 20:39:20 -0700

    fixed broken tests and upgraded createTransaction

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ const Blockchain = require('../blockchain');
 const P2PServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -12,6 +13,7 @@ const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
 const p2pServer = new P2PServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(bodyParser.json());
 
@@ -38,8 +40,14 @@ app.post('/transact', (req, res) => {
   res.redirect('/transactions');
 });
 
+app.get('/mine-transactions', (req, res) => {
+  const block = miner.mine();
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks');
+});
+
 app.get('/public-key', (req, res) => {
-  res.json({ publicKey: wallet.publicKey }); 
+  res.json({ publicKey: wallet.publicKey });
 });
 
 app.listen(HTTP_PORT, () => {
